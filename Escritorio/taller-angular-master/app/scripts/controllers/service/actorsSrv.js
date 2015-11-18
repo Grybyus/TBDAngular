b2bc6033da057dd969bc52a9e999b18e8fa35643
Grybyus <david.calistro@usach.cl>
2015-11-18 11:28:14 -0300

    Update actorsSrv.js

--- Escritorio/taller-angular-master/app/scripts/controllers/service/actorsSrv.js
+++ Escritorio/taller-angular-master/app/scripts/controllers/service/actorsSrv.js
@@ -4,4 +4,7 @@ angular.module('angularSpa')
         this.getActors = function(){
             return $http.get(urlBase);
         };
+        var urlBaseE = 'http://grybyus:8080/sakila-backend-master/actors/';
+ 	    this.getActor = function(id){
+            return $http.get(urlBaseE.concat(id));}	;
     });
